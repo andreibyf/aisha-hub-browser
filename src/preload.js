b7275f7a6afb46bd3d652f76473c814c6d7ba14e
Andrei Byfield <andreibyf@yahoo.com>
2025-09-20 14:46:45 -0400

    Javascript to Typescript

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-// src/preload.ts
-import { contextBridge } from 'electron';
-import { startAgui, stopAgui } from './agent/agui-client';
-
-contextBridge.exposeInMainWorld('agui', {
-  start: (url: string, opts?: { showPanel?: boolean }) => startAgui(url, opts),
-                                stop: () => stopAgui()
-});
-
